9dd4b7ebd109f9c32c6a2e27a721e81a3f69fe50
Slava Panishev <panishev11@gmail.com>
2021-04-02 00:20:25 +0300

    Закомитил всеÐ

---
+++ 01/app.js
@@ -0,0 +1,16 @@
+'use strict';
+
+let money;
+
+let income;
+
+let addExpenses;
+
+let deposit;
+
+let mission;
+
+let period;
+
+alert('У меня не получилось соеденить GIT c PHPStorm');
+console.log(2);
