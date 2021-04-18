e1b2f07170f3f229d51e00b0a08894c88fe7bf49
Slava Panishev <panishev11@gmail.com>
2021-04-18 17:47:45 +0300

    4 задание - удалил окно рекламы из DOM

--- 10/app.js
+++ 10/app.js
@@ -8,6 +8,8 @@
 // в шестой книге добавить главу “Глава 8: За пределами ES6” и поставить её в правильное место
 
 const book = document.querySelectorAll('.book');
+const ad = document.querySelector('.adv');
+
 book[5].after(book[2]);
 book[1].after(book[0]);
 book[0].after(book[4]);
@@ -17,3 +19,5 @@ book[0].after(book[4]);
 document.body.style.backgroundImage = 'url(./image/you-dont-know-js.jpg)';
 
 book[4].innerHTML = '<h2><a href="https://github.com/azat-io/you-dont-know-js-ru/blob/master/this%20%26%20object%20prototypes/README.md#you-dont-know-js-this--object-prototypes" target="_blank">Книга 3. this и Прототипы Объектов</a></h2>';
+
+ad.remove();
